9896c18f99116746f48f0da7e2921bc6bbdfe27f
Cezar Cruz <cezarcruz@outlook.com>
2019-12-06 16:54:26 -0300

    wip: initial base with broker

--- src/main/java/com/fleetlize/rating/gateway/jms/BookCreationListener.java
+++ src/main/java/com/fleetlize/rating/gateway/jms/BookCreationListener.java
@@ -1,13 +1,10 @@
 package com.fleetlize.rating.gateway.jms;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fleetlize.rating.gateway.jms.incoming.BookCreation;
 import com.fleetlize.rating.gateway.jms.mapper.JmsBookMapper;
 import com.fleetlize.rating.usecase.CreateBook;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -15,20 +12,15 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class BookCreationListener {
 
-  private ObjectMapper objectMapper;
   private CreateBook createBook;
   private JmsBookMapper jmsBookMapper;
 
-  @JmsListener(destination = "${fleetlize.jms.topic.book-creation}")
-  public void messageReceived(final String bookCreation) {
+  public void messageReceived(final BookCreation bookCreation) {
     log.info("book creation notified {}", bookCreation);
 
-    try {
-      final var bookCreationObject = objectMapper.readValue(bookCreation, BookCreation.class);
-      createBook.execute(jmsBookMapper.from(bookCreationObject));
-    } catch (JsonProcessingException e) {
-      log.error("error parsing notification", e);
-    }
+    final var booking = createBook.execute(jmsBookMapper.from(bookCreation));
+    log.debug("booking created {}", booking);
+
   }
 
 }
